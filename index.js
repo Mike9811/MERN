ca2b868a4db9043a5870e49612e9691a5305e7e5
mike98 <extraterrestrialr98@gmail.com>
2020-09-18 15:12:02 +0900

    2020/09/18 Updat

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const app = express();
 const port = 3000;
 const bodyParser = require("body-parser");
 const config = require("./config/key.js");
+const cookieParser = require("cookie-parser");
 const { User } = require("./models/User.js");
+const { auth } = require("./middleware/auth.js");
 
 // application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 // application/json
 app.use(bodyParser.json());
+
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 mongoose
@@ -27,12 +31,64 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.post("/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
   const user = new User(req.body);
-
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
+app.post("/api/users/login", (req, res) => {
+  // 요청받은 이메일을 DB에 있는지 확인
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+    // 요청된 이메일이 DB에 있다면 비밀번호가 맞는 비밀번호인지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+
+      // 비밀번호가 맞다면 token 생성
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        // 토큰을 저장한다. 어디에 ? 쿠키, 로컬스토리지
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
+  });
+});
+
+app.get("/api/users/auth", auth, (req, res) => {
+  // 여기까지 미들웨어를 통과해 왔다는 애기는 Authentication이 True라는 말.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true, // role = 1이면 어드민 0이면 권한없음 2는 다른부서 (이런식으로 나누어줄 수 있음)
+    name: req.user.name,
+    isAuth: true,
+    email: req.user.email,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
+});
+
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
       success: true,
     });
   });
